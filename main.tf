2c3502583b61fce3a31232a6979e2fa0c45bf696
Oliver Sterzel <959372+osterzel@users.noreply.github.com>
2025-12-18 12:55:10 +0000

    WIP

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 data "aws_iam_policy_document" "super_expansive" {
   statement {
     effect = "Allow"
-    actions = [ "*" ]
-    resources = [ "*" ]
+    actions = [ "s3:GetObject" ]
+    resources = [ "arn:aws:s3:::somebucket/someprefix/*" ]
   }
 }
